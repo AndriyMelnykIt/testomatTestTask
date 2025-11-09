3b0f5522fdd97260c2814db47fcc33e13b5b9f8d
AndriyMelnykIt <andriy523625@gmail.com>
2025-11-09 17:10:45 +0200

    fix: edit server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,23 @@
 const { app } = require('../BE/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+	try {
+		const originalUrl = req.url || req.path || '/';
+		
+		let expressPath = originalUrl;
+		if (expressPath.startsWith('/api')) {
+			expressPath = expressPath.replace(/^\/api/, '') || '/';
+		}
+		
+		req.url = expressPath;
+		req.path = expressPath;
+		req.originalUrl = expressPath;
+		
+		return app(req, res);
+	} catch (error) {
+		console.error('Serverless function error:', error);
+		if (!res.headersSent) {
+			res.status(500).json({ error: 'Internal server error', message: error.message });
+		}
+	}
+};
